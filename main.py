987df44cb8fbaeebea69c20729e9e2cdc59cafbe
Taranau <taranau.blaidd@gmail.com>
2026-02-19 17:42:16 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import logging
 import requests
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
+from telegram.ext import (
+    ApplicationBuilder, 
+    ContextTypes, 
+    CommandHandler, 
+    ConversationHandler, 
+    MessageHandler, 
+    filters)
 from typing import Optional, TypeAlias
+import re
+import datetime
 
 from PetCalendarBot.hidden_keys import API_KEYS, PERMITTED_CHAT_IDS, DESTINATION_CHAT
 from PetCalendarBot.common.types import (
@@ -15,6 +23,7 @@ from PetCalendarBot.common.types import (
 from PetCalendarBot.common.pronouns import PronounFactory
 import PetCalendarBot.telegram_messages as messages
 import PetCalendarBot.database_processes as database
+import PetCalendarBot.calendar_processes as calendar
 
 Response: TypeAlias = requests.Response
 
@@ -22,108 +31,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.WARNING
 )
-
-def _RebracketArgs(input:list[str]) -> list[str]:
-    rejoined_input: list[str] = []
-    start: int|None = None
-    end: int|None = None
-    aux_list: list[str] = []
-    # This short bit of logic will turn e.g. 
-    # ["(Name:long", "name)", "City:city", "(Links:Fetlife:link", "Telegram:link)"]
-    # into
-    # ["Name:long name", "City:city", "Links:Fetlife:link Telegram:link"]
-    for index, arg in enumerate(input):
-        if arg[0] == '(':
-            start = index
-        if arg[-1] == ')':
-            end = index
-        
-        if start is None:
-            rejoined_input.append(arg)
-        else:
-            aux_list.append(arg)
-        if not end is None:
-            rejoined_input.append((" ".join(aux_list))[1:-1])
-            start, end = None, None
-            aux_list = []
-    return rejoined_input
- 
-def _SplitArgs(input: list[str]) -> dict[str,str]:
-    return {pair[0]: pair[1] for pair in [arg.split(':', 1) for arg in input]}
-
-def _ProcessLinkBlob(link_blob:list[str], pair_split_char: str = ':') -> EventLinks|None:
-    link_dict: dict[str,str] = {pair[0]: pair[1] for pair in [link.split(pair_split_char, 1) for link in link_blob]}
-    if not link_dict:
-        return None
-    event_links: EventLinks = {'telegramChatLink': link_dict.get('Telegram'),
-                               'fetlifeLink': link_dict.get('Fetlife'),
-                               'otherSocialLink': link_dict.get('OtherSocial'),
-                               'otherLink': link_dict.get('Other')}
-    return event_links
-
-def _ProcessContactBlob(contact_blob:list[str], pair_split_char: str = ':') -> PersonDetails|None:
-    contact_dict: dict[str,str] = {pair[0]: pair[1] for pair in [link.split(pair_split_char, 1) for link in contact_blob]}
-    
-    if (name:= contact_dict.get('Name')) is None:
-        return None
-    if (unformatted_pronouns:=contact_dict.get('Pronouns')) is None:
-        return None
-    else:
-        pronouns: str = PronounFactory(unformatted_pronouns.split('/'))
-    
-    contact: PersonDetails = {'name': name,
-                              'pronouns': pronouns,
-                              'blueskyHandle': contact_dict.get('Bluesky'),
-                              'telegramHandle': contact_dict.get('Telegram'),
-                              'fetlifeID': contact_dict.get('Fetlife')}
-    return contact
-
-def _ProcessEventInput(input:list[str]) -> EventDetails|None:
-    rejoined_input = _RebracketArgs(input)
-    args = _SplitArgs(rejoined_input)
-
-    name: str|None = args.get('Name')
-    city: str|None = args.get('City')
-
-    if name is None or city is None:
-        return None
-
-    venue_name: Optional[str] = args.get('Venue')
-    gmaps_link: Optional[str] = args.get('AddressLink')
-    # Currently, the only API I know for this charges for the service.
-
-    contact_blob: Optional[str] = args.get('Contact')
-    contact: PersonDetails|None = None
-    if contact_blob:
-        contact = _ProcessContactBlob(contact_blob.split(' '))
-
-    link_blob: Optional[str] = args.get('Links')
-    event_links: EventLinks|None = None
-    if link_blob:
-        event_links = _ProcessLinkBlob(link_blob.split(' '))
-
-    event_details: EventDetails = {'name': name,
-                                   'acronym': args.get('Acronym'),
-
-                                   'city': city,
-                                   'venue': venue_name,
-                                   'addressLink': gmaps_link,
-                                   
-                                   'contact':contact,
-                                   'organisers':[],
-
-                                   'links': event_links}
-
-    return event_details
-
-
-def _PreprocessTelegramUpdate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> tuple[int|None, list[str]|None]:
-    assert(not (chat := update.effective_chat) is None)
-    if not (feedback_id:=chat.id) in PERMITTED_CHAT_IDS:
-        return None, None
-    if (unsplit_args:=context.args) is None:
-        return feedback_id, None
-    return feedback_id, unsplit_args
 
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
     '''Sends a telegram message explaining bot commands.
@@ -228,7 +135,7 @@ class NewEvent:
             context.user_data[var_name] = update.message.text
         if keyboard:
             await update.message.reply_text(message, 
-                                            reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+                                            reply_markup=ReplyKeyboardMarkup([keyboard], one_time_keyboard=True)
                                             )
         else:
             await update.message.reply_text(message, reply_markup=ReplyKeyboardRemove())
@@ -261,7 +168,7 @@ class NewEvent:
             return ACRONYM
         message = '''What is the event's location? \n
         This can be online, UK-wide, or a specific town, city or region.'''
-        await NewEvent._SaveDataAndReply(update, context, message, "event_acronym", ["Online", "UK-wide"])
+        await NewEvent._SaveDataAndReply(update, context, message, var_name="event_acronym", keyboard=["Online", "UK-wide"])
         return LOCATION
     
     @staticmethod
@@ -295,25 +202,24 @@ class NewEvent:
     @staticmethod
     async def ContactName(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         message = '''What platform are they on?\n
-        For instance, Telegram, FetLife, WhatsApp.'''
+        For instance, Telegram, FetLife, Bluesky.'''
         await NewEvent._SaveDataAndReply(update, context, message, var_name="contact_name")
         return CONTACT_PLATFORM
     
     @staticmethod
     async def ContactPlatform(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         message = ''
-        assert(not (update.message is None or context.user_data is None))
-        platform: str | None = update.message.text
+        assert(not (update.message is None or context.user_data is None or update.message.text is None))
+        platform: str = update.message.text.lower()
         match platform:
-            case "Telegram":
+            case "telegram":
                 message = '''What is their Telegram handle?\n
                 For example, Taranau is "@aaderyn"'''
-            case "FetLife":
+            case "fetlife":
                 message = '''What is their FetLife handle?\n
                 For example, Taranau is "Taranau"'''
-            case "WhatsApp":
-                message = '''What is their phone number?'''
-                raise NotImplementedError
+            case "bluesky":
+                message = '''What is their Bluesky handle?'''
             case _:
                 message = '''Please provide a link to their profile.'''
         await NewEvent._SaveDataAndReply(update, context, message, var_name='contact_platform')
@@ -395,64 +301,97 @@ class NewEvent:
         return ADD_TO_DATABASE
 
     @staticmethod
+    def _ProcessEventInfo(update: Update, 
+                                context: ContextTypes.DEFAULT_TYPE) -> EventDetails:
+        assert(not context.user_data is None)
+        contact: Optional[PersonDetails] = None
+        if contact_name := context.user_data.get('contact_name'):
+            contact = {'name': contact_name,
+                       'pronouns': context.user_data['pronouns'],
+                       'blueskyHandle': None,
+                       'fetlifeID': None,
+                       'telegramHandle': None}
+            match context.user_data.get('contact_platform'):
+                case 'telegram':
+                    contact.update({'telegramHandle': context.user_data.get('contact_handle_or_link')})
+                case 'fetlife':
+                    contact.update({'fetlifeID': context.user_data.get('contact_handle_or_link')})
+                case 'bluesky':
+                    contact.update({'blueskyHandle': context.user_data.get('contact_handle_or_link')})
+
+        links: EventLinks = {'telegramChatLink': None,
+                             'fetlifeLink': None,
+                             'otherSocialLink': None,
+                             'otherLink': None,
+                             'ticketLink': context.user_data.get('ticket_link')}
+        event_link = context.user_data.get('event_link')
+        if event_link:
+            if re.search('.*(t\.me/|telegram\.org).*', event_link):
+                links.update({'telegramChatLink': event_link})
+            elif re.search('.*fetlife\.com.*', event_link):
+                links.update({'fetlifeLink': event_link})
+            elif re.search('.*(whatsapp\.com|instagram\.com).*', event_link):
+                links.update({'otherSocialLink': event_link})
+            else:
+                links.update({'otherLink': event_link})
+
+        event: EventDetails = {'name': context.user_data['event_name'],
+                               'acronym': context.user_data.get('event_acronym'),
+                               
+                               'city': context.user_data['location'],
+                               'venue': context.user_data.get('venue'),
+                               'addressLink': None,
+                               
+                               'contact': contact,
+                               'organisers': None,
+                               
+                               'ticketPrice': context.user_data.get('ticket_cost'),
+                               
+                               'links': links}
+        return event
+
+    @staticmethod
     async def AddToDatabase(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        assert(not update.message is None)
-        if update.message.text == "Yes":
-            #Do a thing here
-            raise NotImplementedError
-        
-        raise NotImplementedError
+        message = '''Would you like to add this event to the calendar?'''
+        assert(not (update.message is None or update.message.text is None))
+        event_info: EventDetails = NewEvent._ProcessEventInfo(update, context)
+        if update.message.text.capitalize() == "Yes":
+            database.ManageEvent.New(event_info)    
+        await NewEvent._SaveDataAndReply(update, context, message, keyboard=["Yes","No"])    
         return ADD_TO_CALENDAR
     
     @staticmethod
     async def AddToCalendar(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        raise NotImplementedError
+        message = '''Would you like to add this event to the map? (dev note: feature not currently implemented)'''
+        assert(not context.user_data is None)
+        assert(not (update.message is None or update.message.text is None))
+        if update.message.text.capitalize() == "Yes":
+            date: Optional[str] = context.user_data.get('initial_timings')
+            #Should be in format DD-MM-YYYY, HH:MM-HH:MM
+            if date:
+                event_info: EventDetails = NewEvent._ProcessEventInfo(update, context)
+                day, times = date.split(',')
+                start_time, end_time = times.strip().split("-")
+                start_date_obj = datetime.datetime.strptime(day + " " + start_time, "%d-%m-%Y %H:%M")
+                end_date_obj = datetime.datetime.strptime(day + " " + end_time, "%d-%m-%Y %H:%M")
+                calendar.AddEventToCalendar(start_date_obj, end_date_obj, event_info)
+            else:
+                message = "Oh no! You didn't provide the initial timings!" + message
+        await NewEvent._SaveDataAndReply(update, context, message)
+        return ADD_TO_MAP
 
     @staticmethod
     async def AddToMap(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        raise NotImplementedError
+        message = '''Would you like to send an update message on telegram? (dev note: currently just sends to this chat)'''
+        await NewEvent._SaveDataAndReply(update, context, message, keyboard=["Yes", "No"])
+        return SEND_UPDATE
     
     @staticmethod
     async def SendUpdateMessage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-        raise NotImplementedError
-
-    @staticmethod
-    async def NewEventReplacement(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        feedback_id, _ = _PreprocessTelegramUpdate(update, context)
-        if feedback_id is None:
-            return None
-        # First: ask for name of event
-        # Second: ask for acronym
-        ## Substep: check for acronym clash? Complicated logic there
-        # Third: ask for city
-        # Fourth: ask for venue
-        # Fifth: ask about event contact
-        ## Substeps: name, pronouns, telegram handle if applicable
-        # Sixth: ask about links
-        # ```
-        # /NewEvent \(Name:Leicestershire Kennel Pups\) Acronym:LKP
-        # City:Leicester \(Venue:Helsinki, 94 Rutland St, LE1 1SB\)
-        # \(Contact:Name:Xadia Pronouns:He Telegram:@Pupxadia\)
-        # \(Links:Telegram:https://t\.me/AllThePups\)```
-        return None
-
-async def NewEventOld(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    feedback_id, unsplit_args = _PreprocessTelegramUpdate(update, context)
-    if unsplit_args is None or feedback_id is None:
-        return None
-    message: MarkdownString = ''
-    chat_id: int = feedback_id
-    event_details = _ProcessEventInput(unsplit_args)
-    if event_details is None:
-        message = "Missing event name or city\."
-    else:
-        message=messages.NewEvent(event_details)
-        database.ManageEvent.New(event_details)
-        chat_id = DESTINATION_CHAT
-    await context.bot.send_message(chat_id=chat_id, text=message, parse_mode='MarkdownV2')
-
-def NewEventHandlerFactory():
-    return None
+        event_details: EventDetails = NewEvent._ProcessEventInfo(update, context)
+        message = messages.NewEvent(event_details)
+        await NewEvent._SaveDataAndReply(update, context, message)
+        return ConversationHandler.END
 
 def _UpdateEventCaseSelect(command: str, 
                               args: dict[str, str], 
@@ -554,22 +493,53 @@ async def Meta(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     await context.bot.send_message(chat_id=DESTINATION_CHAT, text=message, parse_mode='MarkdownV2')
 
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
 def RunBot(TELEGRAM_KEY: str) -> None:
     application = ApplicationBuilder().token(TELEGRAM_KEY).build()
 
     help_handler = CommandHandler('help', help)
     application.add_handler(help_handler)
 
-    new_event_handler = CommandHandler('NewEvent', NewEvent.NewEvent)
+    # update_event_handler = CommandHandler('UpdateEvent', UpdateEvent)
+    # application.add_handler(update_event_handler)
+ 
+    # meta_handler = CommandHandler('Meta', Meta)
+    # application.add_handler(meta_handler)
+
+    new_event_handler = ConversationHandler(
+        entry_points=[CommandHandler("NewEvent", NewEvent.NewEvent)],
+        states={EVENT_NAME: [MessageHandler(filters.TEXT, NewEvent.EventName)],
+                ACRONYM: [MessageHandler(filters.TEXT, NewEvent.Acronym), CommandHandler("skip", NewEvent.SkipAcronym)],
+                LOCATION: [MessageHandler(filters.TEXT, NewEvent.Location)],
+                VENUE: [MessageHandler(filters.TEXT, NewEvent.Venue), CommandHandler("skip", NewEvent.SkipVenue)],
+                CONTACT_NAME: [MessageHandler(filters.TEXT, NewEvent.ContactName), CommandHandler("skip", NewEvent.SkipContact)],
+                CONTACT_PLATFORM: [MessageHandler(filters.TEXT, NewEvent.ContactPlatform)],
+                CONTACT_HANDLE: [MessageHandler(filters.TEXT, NewEvent.ContactHandle)],
+                CONTACT_PRONOUNS: [MessageHandler(filters.TEXT, NewEvent.ContactPronouns)],
+                INITIAL_TIMINGS: [MessageHandler(filters.TEXT, NewEvent.InitialTimings), CommandHandler("skip", NewEvent.SkipInitialTimings)],
+                TICKET_COST: [MessageHandler(filters.TEXT, NewEvent.TicketCost), CommandHandler("skip", NewEvent.SkipTickets)],
+                TICKET_LINK: [MessageHandler(filters.TEXT, NewEvent.TicketLink)],
+                EVENT_LINK: [MessageHandler(filters.TEXT, NewEvent.EventLink)],
+                ADD_TO_DATABASE: [MessageHandler(filters.TEXT, NewEvent.AddToDatabase)],
+                ADD_TO_CALENDAR: [MessageHandler(filters.TEXT, NewEvent.AddToCalendar)],
+                ADD_TO_MAP: [MessageHandler(filters.TEXT, NewEvent.AddToMap)],
+                SEND_UPDATE: [MessageHandler(filters.TEXT, NewEvent.SendUpdateMessage)],
+        },
+        fallbacks = [CommandHandler("cancel", cancel)],
+    )
     application.add_handler(new_event_handler)
 
-    update_event_handler = CommandHandler('UpdateEvent', UpdateEvent)
-    application.add_handler(update_event_handler)
- 
-    meta_handler = CommandHandler('Meta', Meta)
-    application.add_handler(meta_handler)
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
-    application.run_polling()
 
 if __name__ == '__main__':
     RunBot(API_KEYS["TELEGRAM_KEY"])
